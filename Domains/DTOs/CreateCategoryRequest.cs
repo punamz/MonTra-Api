e9e2acf651d7f19be6fbc8531580bd7b1095400f
Nam Nguyễn Phương <nguyenphuongnam.pascal@gmail.com>
2024-01-26 22:40:17 +0700

    add api get and create category

--- Domains/DTOs/CreateCategoryRequest.cs
+++ Domains/DTOs/CreateCategoryRequest.cs
@@ -5,5 +5,5 @@ namespace MonTraApi.Domains.DTOs;
 public class CreateCategoryRequest
 {
     public CategoryType Type { get; set; } = CategoryType.Expenses;
-    public string Category { get; set; } = null!;
+    public string? Category { get; set; }
 }
